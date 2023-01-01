2d5cbed3cf33603259a6e9dd4d00c8e886c03d63
devhak <devhak.enzo@gmail.com>
2023-01-01 21:26:38 +0900

    HttpServeltRequest의 header정보 조회 코드 추가

---
+++ servlet 2/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.request;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.service(request, response);
+    }
+
+    //Header 모든 정보
+    private void printHeaders(HttpServletRequest request){
+        System.out.println("---Headers - start ----");
+
+        
+    }
+
+
+}
