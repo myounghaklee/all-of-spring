7926fef8a91252691682fcda49aeee21fb9aed6b
devhak <devhak.enzo@gmail.com>
2023-01-02 21:40:19 +0900

    request를 사용하여 header정보확인

--- servlet 2/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet 2/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
@@ -15,13 +16,21 @@ public class RequestHeaderServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         super.service(request, response);
+        printHeaders(request);
     }
 
     //Header 모든 정보
     private void printHeaders(HttpServletRequest request){
         System.out.println("---Headers - start ----");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName + ": " + headerName);
+        }
+        System.out.println("---Headers - end----");
+        System.out.println();
 
-        
+
     }
 
 
