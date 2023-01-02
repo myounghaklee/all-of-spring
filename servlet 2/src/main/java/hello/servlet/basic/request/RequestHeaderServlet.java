0eb9507edb076391fe5a7ac168af778a4511e152
devhak <devhak.enzo@gmail.com>
2023-01-02 21:52:39 +0900

    request heaer의 util성 정보 확인 코드추가

--- servlet 2/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet 2/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -17,16 +17,36 @@ public class RequestHeaderServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         super.service(request, response);
         printHeaders(request);
+        printHeaderUtils(request);
     }
 
-    //Header 모든 정보
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("---Header 편의 조회 Start ----");
+        System.out.println("[HOst 편의 조회 ]");
+        System.out.println("request.getServername() = :" + request.getServerName());//host header
+        System.out.println("request.getServerPort() = :" + request.getServerPort()); // host header
+        System.out.println();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = : " + locale));
+        System.out.println("request.getLocalie() = " + request.getLocales());
+        System.out.println();
+
+
+    }
+
+    //ader 모든 정보
     private void printHeaders(HttpServletRequest request){
         System.out.println("---Headers - start ----");
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()){
-            String headerName = headerNames.nextElement();
-            System.out.println(headerName + ": " + headerName);
-        }
+//        Enumeration<String> headerNames = request.getHeaderNames();
+//        while (headerNames.hasMoreElements()){
+//            String headerName = headerNames.nextElement();
+//            System.out.println(headerName + ": " + headerName);
+//        }
+
+        request.getHeaderNames().asIterator()
+                        .forEachRemaining(headerName -> System.out.println(headerName + " : " + headerName));
         System.out.println("---Headers - end----");
         System.out.println();
 
