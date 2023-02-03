e8558a86c08a6b441d35d53533679e34281ff074
devhak <devhak.enzo@gmail.com>
2023-02-03 23:15:00 +0900

    controller 기본 process  생성

--- servlet 2/src/main/java/hello/servlet/web/frontController/v1/controller/MemberFormControllerV1.java
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v1/controller/MemberFormControllerV1.java
@@ -1,6 +1,7 @@
 package hello.servlet.web.frontController.v1.controller;
 
 import hello.servlet.web.frontController.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +11,8 @@ import java.io.IOException;
 public class MemberFormControllerV1 implements ControllerV1 {
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
     }
 }
