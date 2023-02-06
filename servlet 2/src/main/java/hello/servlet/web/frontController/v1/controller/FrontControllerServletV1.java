d8a0958077f9d0e7f10ce1c870c3e6ba554d36ed
devhak <devhak.enzo@gmail.com>
2023-02-06 23:19:22 +0900

    list조회 컨트롤러, frontcontroller 생성

---
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v1/controller/FrontControllerServletV1.java
@@ -0,0 +1,32 @@
+package hello.servlet.web.frontController.v1.controller;
+
+
+import hello.servlet.web.frontController.v1.ControllerV1;
+import hello.servlet.web.frontController.v1.MemberListControllerV1;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1() {
+        //"url" 로 들어오면 mew Controller로 연동된다.
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+}
