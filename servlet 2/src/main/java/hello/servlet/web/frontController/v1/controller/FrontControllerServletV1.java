f279c25fc2099b7a24ab56766b9bad508d54ee24
devhak <devhak.enzo@gmail.com>
2023-02-06 23:42:30 +0900

    frontController 로직 생성

--- servlet 2/src/main/java/hello/servlet/web/frontController/v1/controller/FrontControllerServletV1.java
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v1/controller/FrontControllerServletV1.java
@@ -19,6 +19,7 @@ public class FrontControllerServletV1 extends HttpServlet {
 
     public FrontControllerServletV1() {
         //"url" 로 들어오면 mew Controller로 연동된다.
+        //Http 요청이 들어왔을때 호출하는 매핑정보 세팅
         controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
         controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
         controllerMap.put("/front-controller/v1/members/members", new MemberListControllerV1());
@@ -28,6 +29,13 @@ public class FrontControllerServletV1 extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("FrontControllerServlerV1.service");
 
-
+        String requestURI = request.getRequestURI();
+        ControllerV1 controller = controllerMap.get(requestURI);
+        //예외처리
+        if(controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        controller.process(request,response);
     }
 }
