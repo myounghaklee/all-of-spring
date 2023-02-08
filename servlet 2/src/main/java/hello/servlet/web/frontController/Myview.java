2a8bf6f231a937c65944b48ed50733624876e2f3
devhak <devhak.enzo@gmail.com>
2023-02-08 23:04:01 +0900

    view forwarding 코드 추가 - controller v2 추가

--- servlet 2/src/main/java/hello/servlet/web/frontController/Myview.java
+++ servlet 2/src/main/java/hello/servlet/web/frontController/Myview.java
@@ -1,16 +1,23 @@
 package hello.servlet.web.frontController;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public class Myview {
 
     private String viewPath;
 
-    public Myview(String viewPath){
+    public Myview(String viewPath) {
         this.viewPath = viewPath;
     }
 
-    public void render(HttpServletRequest request, HttpServletResponse response){}
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
 
 }
