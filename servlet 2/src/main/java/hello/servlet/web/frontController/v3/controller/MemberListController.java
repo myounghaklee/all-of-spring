12c810f2374d007417ed8622fa96c6709efdf69c
devhak <devhak.enzo@gmail.com>
2023-02-16 23:41:27 +0900

    member 조회api  생성

--- servlet 2/src/main/java/hello/servlet/web/frontController/v3/controller/MemberListController.java
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v3/controller/MemberListController.java
@@ -5,21 +5,16 @@ import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontController.ModelView;
 import hello.servlet.web.frontController.v3.ControllerV3;
 
+import java.util.List;
 import java.util.Map;
 
-public class MemberSaveControllerV3 implements ControllerV3 {
-
+public class MemberListController implements ControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
     @Override
     public ModelView process(Map<String, String> paramMap) {
-        String userName = paramMap.get("username");
-        int age = Integer.parseInt(paramMap.get("age"));
-
-        Member member = new Member(userName, age);
-        memberRepository.save(member);
-
-        ModelView mv = new ModelView("save-result");
-        mv.getModel().put("member", member);
+        List<Member> memberList = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", memberList);
         return mv;
 
     }
