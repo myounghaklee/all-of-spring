2c27a145a4333ddb797cc7a1c5b66a78e4b62f37
devhak <devhak.enzo@gmail.com>
2023-02-21 23:16:23 +0900

    V4 FrontController 생성

---
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v4/FrontControllerServletV4.java
@@ -0,0 +1,61 @@
+package hello.servlet.web.frontController.v4;
+
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.Myview;
+import hello.servlet.web.frontController.v3.ControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontController.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontController.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontController.v4.controller.MemberSaveControllerV4;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+    private Map<String, ControllerV4> controllerV4Map = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        controllerV4Map.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerV4Map.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerV4Map.put("/front-controller/v4/members/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String requestURI = req.getRequestURI();
+        ControllerV4 controllerV4 = controllerV4Map.get(requestURI);
+
+        if (controllerV4 == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        //paramMap
+        Map<String, String> paramMap = createParamMap(req);
+        Map<String, Object>model = new HashMap<>();
+        String viewName = controllerV4.process(paramMap, model);
+
+        Myview view = viewResolver(viewName);
+
+        view.render(model, req, resp);
+    }
+
+    private static Myview viewResolver(String viewName) {
+        return new Myview("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String> paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
+    }
+}
