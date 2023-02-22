036703a0a1902b336e34c9f8f28ace752cc120a6
devhak <devhak.enzo@gmail.com>
2023-02-22 23:31:18 +0900

    핸들러, 핸들러 어댑터 목록 조회하여 핸들러 호출하는 버전 생성

---
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v5/FrontControllerServletV5.java
@@ -0,0 +1,80 @@
+package hello.servlet.web.frontController.v5;
+
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.Myview;
+import hello.servlet.web.frontController.v3.ControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontController.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontController.v5.adapter.ControllerV3HandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@WebServlet(name = "frontControllerSErvletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
+
+    public FrontControllerServletV5(){
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdapters.add(new ControllerV3HandlerAdapter());
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v3/members/members", new MemberListControllerV3());
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //handler조회
+        Object handler = getHandler(req);
+        if (handler == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //handlerAdapter조회
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+
+        ModelView mv = adapter.handle(req, resp, handler);
+
+        String viewName = mv.getViewName();//논리이름  ex)new-form
+        Myview view = viewResolver(viewName);
+
+        view.render(mv.getModel(), req, resp);
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for(MyHandlerAdapter adapter : handlerAdapters){
+            if(adapter.supports(handler))return adapter;
+        }
+        throw  new IllegalArgumentException("handler adapter is not matched handler =" + handler);
+    }
+
+    private Object getHandler(HttpServletRequest req) {
+        String requestURI = req.getRequestURI();
+        Object handler = handlerMappingMap.get(requestURI);
+        return handler;
+    }
+    private static Myview viewResolver(String viewName) {
+        return new Myview("/WEB-INF/views/" + viewName + ".jsp");
+    }
+}
--- servlet 2/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV3HandlerAdapter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     @Override
@@ -18,6 +20,17 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
-        return null;
+        ControllerV3 controller = (ControllerV3) handler;
+        Map<String, String> paramMap = createParamMap(request);
+
+        return controller.process(paramMap);
+    }
+
+
+    private static Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String> paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
     }
 }
