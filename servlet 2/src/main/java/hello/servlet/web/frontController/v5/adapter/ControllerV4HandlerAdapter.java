f3864f8821887b6ab87c92de8bee529c8252adf3
devhak <devhak.enzo@gmail.com>
2023-02-26 17:06:03 +0900

    v5버전에 v4버전 연결

---
+++ servlet 2/src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,43 @@
+package hello.servlet.web.frontController.v5.adapter;
+
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.v3.ControllerV3;
+import hello.servlet.web.frontController.v4.ControllerV4;
+import hello.servlet.web.frontController.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        //handler가 ControllerV3를 구현한 controller인지 확인
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV4 controller = (ControllerV4) handler;
+        Map<String, String> paramMap = createParamMap(request);
+
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName =controller.process(paramMap, model);
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+        return mv;
+
+    }
+
+
+    private static Map<String, String> createParamMap(HttpServletRequest req) {
+        Map<String, String> paramMap = new HashMap<>();
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, req.getParameter(paramName)));
+        return paramMap;
+    }
+}
