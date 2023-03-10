f4ce15d154deccca1c05040682856b879b43498b
devhak <devhak.enzo@gmail.com>
2023-03-10 23:19:10 +0900

    requestParam을 사용하여 api 호출받는 형식으로 변경

--- servlet 2/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet 2/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -6,7 +6,9 @@ import hello.servlet.domain.member.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -18,21 +20,20 @@ public class SpringMemberControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @RequestMapping("/new-form")
-    public ModelAndView newForm(){
-        return new ModelAndView("new-form");
+    public String newForm() {
+        return "new-form";
     }
 
     @RequestMapping("/save")
-    public ModelAndView save(HttpServletRequest request, HttpServletResponse response){
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+    public String save(@RequestParam("username") String username,
+                             @RequestParam("age")int age,
+                             Model model) {
 
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        ModelAndView mv = new ModelAndView("save-result");
-        mv.addObject("member", member);
-        return mv;
+        model.addAttribute("member', member");
+        return "save-result";
     }
 
     @RequestMapping
