25c798cbff053bc2349ebe3c370a361cd4938523
devhak <devhak.enzo@gmail.com>
2023-03-13 23:25:49 +0900

    requstmapping method옵션 추가

--- servlet 2/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet 2/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,12 +20,12 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping("/save")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(@RequestParam("username") String username,
                        @RequestParam("age") int age,
                        Model model) {
