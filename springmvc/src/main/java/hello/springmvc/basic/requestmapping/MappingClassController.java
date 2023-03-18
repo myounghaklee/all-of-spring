8d52aa0b9840cea446c75f54b0d3f4864f346b04
devhak <devhak.enzo@gmail.com>
2023-03-18 23:02:01 +0900

    .

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,4 +1,23 @@
 package hello.springmvc.basic.requestmapping;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
+
+    @GetMapping
+    public String users(){
+        return "getUsers";
+    }
+
+    @PostMapping
+    public String addUser(){return "post user";}
+
+    @GetMapping("/{userId}")
+    public String finduser(@PathVariable String userId){
+        return "userId is" + userId;
+    }
+
+
 }
