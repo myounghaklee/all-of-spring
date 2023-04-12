cbfc8c51f4cf7b0dac69971e26fefc2abb0f3dae
devhak <devhak.enzo@gmail.com>
2023-04-12 23:31:22 +0900

    json api 추가

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.io.IOException;
@@ -21,9 +22,11 @@ public class ResponseBodyController {
         response.getWriter().write("ok");
     }
 
-    @RequestMapping("/response-body/string-v2")
-    public ResponseEntity<String> responseBodyV2() throws IOException {
-        return new ResponseEntity<>("ok", HttpStatus.OK);
+    @RequestMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyV1() throws IOException {
+        HelloData helloData = HelloData.builder().username("enzo").age(30).build();
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
     }
 
     @ResponseBody
@@ -37,6 +40,14 @@ public class ResponseBodyController {
         HelloData data = new HelloData("hello json v1 controller", 32);
 
         return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2()  {
+        HelloData data = new HelloData("userAr", 32);
+        return data;
     }
 
 }
