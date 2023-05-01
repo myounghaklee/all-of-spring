0836a04945ab275ff9fe3a8e0d0785eb9e0060a3
devhak <devhak.enzo@gmail.com>
2023-05-01 23:11:36 +0900

    item 객체 생성

---
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,24 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Getter @Setter
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(Long id, String itemName, Integer price, Integer quantity) {
+        this.id = id;
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
