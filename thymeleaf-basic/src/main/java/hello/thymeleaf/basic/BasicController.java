f5eea7f1a8ac2cc44f9ba13a5f9179a0c2840da3
devhak <devhak.enzo@gmail.com>
2023-05-16 21:34:49 +0900

    thymeleaf를 위한 기본 컨드롤러 생성

---
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,21 @@
+package hello.thymeleaf.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Ma First Data");
+        return "basic/text-basic";
+    }
+
+}
