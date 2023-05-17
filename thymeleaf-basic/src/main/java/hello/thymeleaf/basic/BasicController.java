f46c32dc7c5d6842485bfbb10469d91cb342040a
devhak <devhak.enzo@gmail.com>
2023-05-18 00:07:19 +0900

    variable 예제 api, html 코드 추가

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -8,7 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -33,11 +36,17 @@ public class BasicController {
         User userA = new User("userA", 10);
         User userB = new User("userB", 20);
 
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
         Map<String, User> map = new HashMap<>();
         map.put("userA", userA);
         map.put("userB", userB);
 
-
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+        return "/basic/variable";
     }
 
     @Data
