6b47d6d9b8f49856b810f6b63988103fe70919d7
devhak <devhak.enzo@gmail.com>
2023-05-24 23:23:17 +0900

    message test 코드

---
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,20 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessag(){
+        String result = ms.getMessage("hello" , null, null);
+        Assertions.assertThat(result).isEqualTo("안녕");
+    }
+}
