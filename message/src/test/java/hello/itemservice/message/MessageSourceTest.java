93e0509288a4114b2bb4d0e4d8e8685ca90cd4a6
devhak <devhak.enzo@gmail.com>
2023-05-25 00:08:31 +0900

    validation proj  기본 생성

--- message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 
 @SpringBootTest
 public class MessageSourceTest {
@@ -17,4 +18,11 @@ public class MessageSourceTest {
         String result = ms.getMessage("hello" , null, null);
         Assertions.assertThat(result).isEqualTo("안녕");
     }
+
+    @Test
+    void notFoundMessageCode(){
+        Assertions.assertThatThrownBy(()-> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
 }
