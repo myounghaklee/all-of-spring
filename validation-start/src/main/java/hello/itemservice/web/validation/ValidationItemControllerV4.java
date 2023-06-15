9bc0b71dc6a228ddad4e4ad2c53a7726b945cb84
devhak <devhak.enzo@gmail.com>
2023-06-16 00:17:10 +0900

    v4 생성

--- validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/validation/v4//items")
 @RequiredArgsConstructor
-public class ValidationItemControllerV3 {
+public class ValidationItemControllerV4 {
 
     private final ItemRepository itemRepository;
 
@@ -26,20 +26,20 @@ public class ValidationItemControllerV3 {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "validation/v3/items";
+        return "validation/v4/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v3/item";
+        return "validation/v4/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
-        return "validation/v3/addForm";
+        return "validation/v4/addForm";
     }
 
     //@PostMapping("/add")
@@ -53,14 +53,14 @@ public class ValidationItemControllerV3 {
         //검증 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
-            return "validation/v3/addForm";
+            return "validation/v4/addForm";
         }
 
         //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4//items/{itemId}";
     }
 
     @PostMapping("/add")
@@ -74,14 +74,14 @@ public class ValidationItemControllerV3 {
         //검증 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
-            return "validation/v3/addForm";
+            return "validation/v4/addForm";
         }
 
         //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
@@ -106,7 +106,7 @@ public class ValidationItemControllerV3 {
         }
 
         itemRepository.update(itemId, item);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4//items/{itemId}";
     }
 
     @PostMapping("/{itemId}/edit")
@@ -124,7 +124,7 @@ public class ValidationItemControllerV3 {
         }
 
         itemRepository.update(itemId, item);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4//items/{itemId}";
     }
 
 }
