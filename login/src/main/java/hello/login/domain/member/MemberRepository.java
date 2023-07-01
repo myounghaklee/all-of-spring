8fe111809e3a826d6cf74344aa3b3422e61bbbbd
devhak <devhak.enzo@gmail.com>
2023-07-01 23:44:56 +0900

    member 객체, repo 개발

---
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.login.domain.member;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); // static 사용
+}
