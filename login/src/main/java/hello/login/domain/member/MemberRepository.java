56f755a6c66ba3ade92b9f8357f9a53d8cb7e6c8
devhak <devhak.enzo@gmail.com>
2023-07-02 22:55:40 +0900

    repository개발

--- login/src/main/java/hello/login/domain/member/MemberRepository.java
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -4,12 +4,33 @@ package hello.login.domain.member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Repository
 public class MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>(); // static 사용
+    private static long sequence = 0L; // static 사요ㅕㅇ
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        log.info("saved memver ={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findByid(Long id ){
+        return store.get(id);
+    }
+
+    public Optional<Member> findByloginId(String loginId){
+
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+
 }
