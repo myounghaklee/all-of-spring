780bbf2966037236d193d37f834b81ecc6997982
devhak <devhak.enzo@gmail.com>
2023-07-03 23:24:23 +0900

    findByloginId 메서드 lambda 수정

--- login/src/main/java/hello/login/domain/member/MemberRepository.java
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -25,7 +25,17 @@ public class MemberRepository {
     }
 
     public Optional<Member> findByloginId(String loginId){
+//        List<Member> all = findAll();
+//        for(Member m : all){
+//            if(m.getLoginId().equals(loginId)){
+//                return Optional.of(m) ;
+//            }
+//        }
+//        return Optional.empty();
 
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
     }
 
     public List<Member> findAll(){
