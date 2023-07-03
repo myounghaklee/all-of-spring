e183e910b6d6d2ff654ed0d7b883dcc342fff426
devhak <devhak.enzo@gmail.com>
2023-07-03 23:50:50 +0900

    membercontroller 생성

---
+++ login/src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,33 @@
+package hello.login.web.member;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member){
+        return "member/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "members/addMemberForm";
+        }
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+}
