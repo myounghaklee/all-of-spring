d92072d30f19e2c68c694bb470e4eb27ce46a544
devhak <devhak.enzo@gmail.com>
2023-07-05 22:54:03 +0900

    login구현

---
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,35 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password){
+        /**
+         * 주석 코드 == 아래코드 
+         */
+//        Optional<Member> findMemeberOptional = memberRepository.findByloginId(loginId);
+//        Member member = findMemeberOptional.get();
+//        if(member.getPassword().equals(password)){
+//            return member;
+//        }else{
+//            return null;
+//        }
+
+        Optional<Member> byloginId = memberRepository.findByloginId(loginId);
+        return byloginId.filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+}
