5b424862825057cac9abf756a0d5f939b5c5f143
devhak <devhak.enzo@gmail.com>
2023-07-08 23:00:26 +0900

    쿠키 생성

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +28,8 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult,
+                        HttpServletResponse response) {
         if(bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -36,6 +40,10 @@ public class LoginController {
             return "login/loginForm";
         }
         //TODO : 상공처리
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId",
+                String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
 
