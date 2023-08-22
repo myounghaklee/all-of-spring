012ab388dfc78b6ce604f786b6bcb7ab9c9daa16
devhak <devhak.enzo@gmail.com>
2023-08-22 23:49:43 +0900

    session 정보조회 api

--- login/src/main/java/hello/login/web/session/SessionInfoController.java
+++ login/src/main/java/hello/login/web/session/SessionInfoController.java
@@ -1,4 +1,17 @@
 package hello.login.web.session;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
 public class SessionInfoController {
+
+    @GetMapping("session-info")
+    public String sessionInfo(HttpServletRequest request){
+        
+    }
 }
