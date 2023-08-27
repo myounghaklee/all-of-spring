b906320103270dba5e43adbe522b461ba5790e40
devhak <devhak.enzo@gmail.com>
2023-08-27 22:25:28 +0900

    테스트용코드

--- login/src/main/java/hello/login/web/session/SessionInfoController.java
+++ login/src/main/java/hello/login/web/session/SessionInfoController.java
@@ -12,6 +12,17 @@ public class SessionInfoController {
 
     @GetMapping("session-info")
     public String sessionInfo(HttpServletRequest request){
-        
+
+    }
+
+
+    public MyReservationResponse retrieveReservation(){
+        //승인 된 예약 조회
+        dropInService.getMyReservation()
+        //해당 box의 dropIn에 다른 user가 예약했는지 조회
+        boxService.
+        //내 예약정보와 다른사람의 예약정보 조합
+
+        //MyReservationResponse로 변환하여 return;
     }
 }
